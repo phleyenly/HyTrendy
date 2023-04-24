d6b7479f29d6dbc5340768909538f6c08c2f2a15
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-04-24 16:17:30 +0700

    api-type

--- src/main/java/com/lina/HyTrendy/api/Type.java
+++ src/main/java/com/lina/HyTrendy/api/Type.java
@@ -1,7 +1,5 @@
 package com.lina.HyTrendy.api;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +16,9 @@ public class Type {
 	private final TypeService typeService;
 	
 	@GetMapping("/type/{categoryCode}/{typeCode}")
-	public ResponseEntity<String> getTypeByCode (@PathVariable(name = "categoryCode", required = false) String categoryCode,
+	public String getTypeByCode (@PathVariable(name = "categoryCode", required = false) String categoryCode,
 			@PathVariable(name = "typeCode" , required = false) String typeCode) {
-		String json = typeService.getTypeByCode(categoryCode, typeCode);
-        return new ResponseEntity<>(json,HttpStatus.OK);
+		return typeService.getTypeByCode(categoryCode, typeCode);
 	}
 
 }
