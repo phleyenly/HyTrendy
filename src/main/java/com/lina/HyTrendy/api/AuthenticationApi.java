0d6f5027a369e8d6a482d4a84fd97975601ae7a4
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-05-10 23:25:22 +0700

    Update AuthenticationApi.java

--- src/main/java/com/lina/HyTrendy/api/AuthenticationApi.java
+++ src/main/java/com/lina/HyTrendy/api/AuthenticationApi.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lina.HyTrendy.config.JwtUtil;
-import com.lina.HyTrendy.dto.AuthenticaionResponeDto;
 import com.lina.HyTrendy.dto.LoginDto;
 import com.lina.HyTrendy.service.UserDetailsServiceImpl;
 
