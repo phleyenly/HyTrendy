54595b8e130fdae1de9331e17bcb00f47a22995a
phleyenly <129738023+phleyenly@users.noreply.github.com>
2023-05-30 11:08:57 +0700

    person

--- src/main/java/com/lina/HyTrendy/api/Person.java
+++ src/main/java/com/lina/HyTrendy/api/Person.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -68,5 +70,12 @@ public class Person {
 	@DeleteMapping("/person/{id}")
 	public Map<String, String> deleteById(@PathVariable(name = "id", required = true) Long id) {
 		return personService.deleteById(id);
+	}
+	
+	@GetMapping("person/user")
+	public PersonDto findPersonByUsername() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = auth.getName();
+		return personService.findByUsername(username);
 	}
 }
